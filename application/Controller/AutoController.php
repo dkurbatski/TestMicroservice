be2e5f94f5542cd60f4fb6deb10d92ce3742d8c6
kurbatski <illiaa553@mail.ru>
2024-02-20 20:41:58 +0300

    adding sql

---
+++ application/Controller/AutoController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+class AutoController
+{
+    public function list(Request $request): JsonResponse
+    {
+        global $connect;
+        $data= $connect->query("select * from `auto`")->fetchAll();
+        return new JsonResponse(['auto' => $data], 200);
+    }
+
+    public function add(Request $request)
+    {
+     global $connect;
+     $params = $request->toArray();
+     $connect->query("insert into auto (brand, model, odometer, year, price_amount, price_currency) VALUES ('{$params['brand']}', '{$params['model']}',{$params['odometer']}, {$params['year']}, {$params['price_amount']}, '{$params['price_currency']}')")->fetch();
+    return new JsonResponse([], 200);
+    }
+}
+
--- application/routes.php
+++ application/routes.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Controller\AutoController;
 use App\Controller\TestController;
 
 $router = new AltoRouter();
 
 $router->map('GET', '/example', [TestController::class, 'example']);
+$router->map('GET', '/auto', [AutoController::class, 'list']);
+$router->map('POST', '/auto', [AutoController::class, 'add']);
 
 return $router;
