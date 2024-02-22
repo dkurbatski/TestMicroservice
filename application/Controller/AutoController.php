a6f92f8e0cf1174ae5c20038793cf48c114dec0c
kurbatski <illiaa553@mail.ru>
2024-02-22 19:32:28 +0300

    adding sql

--- application/Controller/AutoController.php
+++ application/Controller/AutoController.php
@@ -9,8 +9,24 @@ class AutoController
 {
     public function list(Request $request): JsonResponse
     {
+        $currency = $request->get('currency');
+        $odometer = $request->get('odometer');
+        if ($currency) {
+            $sql = "SELECT auto.id as id, brand, model, odometer, year, price_amount*r.rate as price_amount, r.to as price_currency, is_actual
+FROM auto
+join rate r on r.`from`=auto.price_currency
+where r.`to`='$currency'";
+            if ($odometer){
+                $sql.=" and odometer<$odometer";
+            }
+        } else {
+            $sql = "select * from `auto`";
+            if ($odometer){
+                $sql.=" where odometer<$odometer";
+            }
+        }
         global $connect;
-        $data = $connect->query("select * from `auto`")->fetchAll();
+        $data = $connect->query($sql)->fetchAll();
         return new JsonResponse(['auto' => $data], 200);
     }
 
