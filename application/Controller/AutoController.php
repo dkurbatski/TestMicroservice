5d08e6937854bbba61eb8fb03d8880e233197b9e
kurbatski <illiaa553@mail.ru>
2024-02-24 19:13:37 +0300

    adding sql

--- application/Controller/AutoController.php
+++ application/Controller/AutoController.php
@@ -11,20 +11,44 @@ class AutoController
     {
         $currency = $request->get('currency');
         $odometer = $request->get('odometer');
+        $fromPrice = $request->get('from_price');
+        $toPrice = $request->get('to_price');
+        $orderBy = $request->get('order_by');
+        $orderDirection = $request->get('order_direction');
+        if (!in_array($orderBy, ["brand", "model", "odometer", "year"]) || !in_array($orderDirection, ["asc", "desc"])) {
+            return new JsonResponse(["error" => "Wrong params"], 422);
+        }
         if ($currency) {
             $sql = "SELECT auto.id as id, brand, model, odometer, year, price_amount*r.rate as price_amount, r.to as price_currency, is_actual
 FROM auto
 join rate r on r.`from`=auto.price_currency
 where r.`to`='$currency'";
-            if ($odometer){
-                $sql.=" and odometer<$odometer";
+            if ($odometer) {
+                $sql .= " and odometer<$odometer";
+            }
+            if ($fromPrice) {
+                $sql .= " and price_amount*r.rate=>$fromPrice";
+            }
+            if ($toPrice) {
+                $sql .= " and price_amount*r.rate<=$toPrice";
             }
         } else {
             $sql = "select * from `auto`";
-            if ($odometer){
-                $sql.=" where odometer<$odometer";
+            if ($odometer) {
+                $sql .= " where odometer<$odometer";
+            }
+            if ($fromPrice) {
+                $sql .= " where fromPrice=>$fromPrice";
+            }
+            if ($toPrice) {
+                $sql .= " where toPrice<=$toPrice";
             }
         }
+        if ($orderBy){
+            $sql.=" ORDER BY '$orderBy' ".($orderDirection ?: "asc");
+        }
+        var_dump($sql);
+        die();
         global $connect;
         $data = $connect->query($sql)->fetchAll();
         return new JsonResponse(['auto' => $data], 200);
