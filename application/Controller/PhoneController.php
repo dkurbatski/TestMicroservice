a77a43d585de5955ddc979689d5df38375c2f913
kurbatski <illiaa553@mail.ru>
2024-02-27 20:09:05 +0300

    adding sql

--- application/Controller/PhoneController.php
+++ application/Controller/PhoneController.php
@@ -10,12 +10,30 @@ class PhoneController
 {
     public function info(Request $request)
     {
-        $phone=$request->get('phone');
-        $countryCode=$request->get('country_code');
-        $client = new \GuzzleHttp\Client();
-        $response = $client->request('GET', "https://api-bdc.net/data/phone-number-validate?number=$phone&countryCode=$countryCode&key={$_ENV['BIG_DATA_CLOUD_API_KEY']}");
+        global $connect;
+        $phone = $request->get('phone');
+        $countryCode = $request->get('country_code');
+        $data = $connect->query("select * from phones where e164='$phone'");
+        if (!$data) {
+            $client = new \GuzzleHttp\Client();
+            $response = $client->request('GET', "https://api-bdc.net/data/phone-number-validate?number=$phone&countryCode=$countryCode&key={$_ENV['BIG_DATA_CLOUD_API_KEY']}");
 
-        $data = json_decode($response->getBody(), true); // '{"id": 1420053, "name": "guzzle", ...}'
+            $dataFromJson = json_decode($response->getBody(), true); // '{"id": 1420053, "name": "guzzle", ...}'
+            if (!$dataFromJson['isValid']) {
+                return new JsonResponse(['error' => 'phone not valid'], 422);
+            }
+            $data=[
+                'e164'=>$dataFromJson['e164Format'],
+                'is_valid'=>$dataFromJson['isValid'],
+                'international'=>$dataFromJson['internationalFormat'],
+                'national'=>$dataFromJson['nationalFormat'],
+                'location'=>$dataFromJson['location'],
+                'type'=>$dataFromJson['lineType'],
+                'country_alpha2'=>$dataFromJson['countryAlpha2']['isoAlpha2'],
+                'country_alpha3'=>$dataFromJson['countryAlpha3']['isoAlpha3']
+            ];
+        }
+
         return new JsonResponse($data, 200);
     }
 }
